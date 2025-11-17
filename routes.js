5011407d96e57ec47db9ab1dddae244487e93b7e
Manu22-cloud-cell <mailtomanojky@gmail.com>
2025-11-17 21:54:41 +0530

    Clean Up code by adding another file routes.js

---
+++ routes.js
@@ -0,0 +1,60 @@
+const fs=require("fs");
+
+const requestHandler=(req,res)=>{
+    const url=req.url;
+    const method=req.method;
+
+    if(req.url==='/'){
+        //read message first
+        fs.readFile('message.txt','utf-8',(err,data)=>{
+        const messages = data ? data.split('\n').filter(m=>m.trim()!==''):[];
+
+           res.setHeader('Content-type','text/html');
+           res.end( `
+            <div>
+                <h2>Messages:</h2>
+                 <ul>
+                    ${messages.map(msg=>`<li>${msg}</li>`).join('')}
+                 </ul>
+            </div>
+
+            <form action="/message" method="POST">
+                <label>Message:</label>
+                <input type="text" name="message"></input>
+                <button type="submit">Add</button>
+            </form>
+            `);
+        });
+    }
+    else if(req.url==='/message' && method==='POST'){
+            let body=[];
+            
+            req.on('data',(chunks)=>{
+                body.push(chunks);
+            });
+
+            req.on('end',()=>{
+                const parsedBody=Buffer.concat(body).toString();//message=Hello
+                const message=parsedBody.split("=")[1];
+
+                // Read existing messages
+                fs.readFile('message.txt','utf-8',(err,data)=>{
+                    let oldMessages=data ? data:"";
+
+                    // Add new message at the TOP
+                    const updatedMessages=message + "\n" + oldMessages;
+
+                    fs.writeFile("message.txt",updatedMessages, (err)=>{
+                    res.statusCode=302; //redirected
+                    res.setHeader('Location','/');
+                    res.end();
+                });
+            });       
+        });
+    }
+    else{
+        res.statusCode=404;
+        res.end("<h1>Page not found</h1>");     
+    }
+}
+module.exports=requestHandler;
